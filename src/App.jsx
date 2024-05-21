58a2829186ef4e82e52d5d3f3c3d09a547cdc155
Reda Grain <grain.reda.solicode@gmail.com>
2024-05-21 17:48:26 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
       <Navbar/>
       <Hero/>
       <Educations/>
-      <Experiences/>
       <Projects/>
+      <Experiences/>
     </div>
   );
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import './Projects.css';
 
 export default function Projects() {
+  const [showDetail, SetshowDetail] = useState(false);
+
+  function showDetails(){
+    showDetail ? SetshowDetail(false) : SetshowDetail(true);
+  }
+
   return (
     <div className="Projects" id='Projects'>
       <div className='title'><h1>Projects</h1></div>
       <div className="Projects_Container">
         <div className="project">
-          <div className="project_image">
+          <div onClick={showDetails} className="project_image">
             <img src="https://d1ub0o53i85pdh.cloudfront.net/uploads/2022/04/1200x700_GTM.webp" alt="project image" />
           </div>
-          <div className="project_content">
+          <div className={showDetail ? 'project_content showDetail' : 'project_content'}>
             <h1>SoliLMS</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt voluptatibus odit accusamus voluptates commodi soluta vitae delectus nobis debitis ducimus?</p>
             <a href="">Details</a>
