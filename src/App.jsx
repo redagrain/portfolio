900c4adc03f34e23d4c263b69d643c2d5aa7deea
Reda Grain <grain.reda.solicode@gmail.com>
2024-06-03 13:26:29 +0100

    add intro animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
 import Hero from './Components/Hero/Hero';
@@ -13,12 +13,29 @@ import Skills from './Components/Skills/Skills';
 
 function App() {
   const [showSidebar, SetshowSidebar] = useState(false);
+  const [introAnimation, setIntroAnimation] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIntroAnimation(false);
+    }, 2900);
+
+    return () => clearTimeout(timer);
+  }, []);
+
 
   function toggleSidebar(){
     SetshowSidebar(!showSidebar);
   }
+  const text = "Welcome to My Portfolio";
   return (
     <div className="App">
+      {introAnimation && (
+        <div className="intro">
+          <div className="mainCard leftCard"></div>
+          <div className="mainCard rightCard"></div>
+        </div>
+      )}
       <Navbar showSidebar={showSidebar} toggleSidebar={toggleSidebar}/>
       <Sidebar showSidebar={showSidebar} toggleSidebar={toggleSidebar}/>
       <Hero/>
