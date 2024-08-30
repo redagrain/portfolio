76d01c93c0c39b3eeae4ab87a117fe7374c351f4
Reda Grain <ach@MacBook-Pro-de-ACH.local>
2024-08-30 19:18:19 +0100

    update portfolio

--- src/Components/Experiences/Experiences.jsx
+++ src/Components/Experiences/Experiences.jsx
@@ -1,34 +1,12 @@
 import Title from '../../MiniComponents/Title/title';
 import './Experiences.css';
 import ExperiencesData from '../../Data/Experiences.json'
-import { useState, useEffect } from 'react';
-
-
 
 function Experiences() {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const [experiencesElement, setexperiencesElement] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    handleScroll();
-  }, []);
-  
-  useEffect(() => {
-    const about = document.querySelector('.Experiences');
-    if (about) {
-      setexperiencesElement(about.getBoundingClientRect().top);
-    }
-  }, []);
-
   return (
     <div className="Experiences" id='Experiences'>
       <Title title={'Experiences'}/>
-      <div className={experiencesElement < scrollPosition+300 ? "Experiences_Container animate" : "Experiences_Container"}>
+      <div className="Experiences_Container">
         {        
           ExperiencesData.Experiences.reverse().map((exp) => (
             <div className="experience">
